3022ea676ddde60077c425ec2be223247655f35b
snicolo <seby.nicol@gmail.com>
2017-03-26 16:03:35 +0200

    test remove keyboard

--- strettobot.py
+++ strettobot.py
@@ -59,9 +59,10 @@ def on_chat_message(msg):
              )
     #tragitti per compagnia 1
     if msg['text'] in ['Messina - Villa S.G.', 'Caronte da Messina a Villa']:
-           bot.sendMessage(chat_id, '\n'.join(file('caronte_messina_villa.html')), parse_mode='HTML', reply_markup=remove_keyboard)
+           bot.sendMessage(chat_id, '\n'.join(file('caronte_messina_villa.html')), parse_mode='HTML', ReplyKeyboardRemove())
     elif msg['text'] in ['Villa S.G. - Messina', 'Caronte da Villa a Messina']:
-           bot.sendMessage(chat_id, '\n'.join(file('caronte_villa_messina.html')), parse_mode='HTML', reply_markup=remove_keyboard)
+           markup = ReplyKeyboardRemove()
+           bot.sendMessage(chat_id, '\n'.join(file('caronte_villa_messina.html')), parse_mode='HTML', reply_markup=markup)
      
     if msg['text']== 'Bluferries':
            bot.sendMessage(chat_id, 'Scegli la tratta',
